ecea41288ed59dd663d46c15ecb712327de4c6f1
Katherine <shaposhnikovakate@yandex.ru>
2022-05-03 17:04:03 +0300

    Добавьте файлы проекта.

---
+++ Ray_tracing/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace CG_Ray_tracing
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(850, 800),
+                Title = "Ray tracing",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
---
+++ Ray_tracing/Window.cs
@@ -0,0 +1,94 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using LearnOpenTK.Common;
+
+
+
+namespace CG_Ray_tracing
+{
+    public class Window : GameWindow
+    {
+        private readonly float[] _vertices =
+        {
+             -1f, -1f,  0f,
+              1f, -1f,  0f,
+              1f,  1f,  0f,
+             -1f,  1f,  0f
+        };
+
+        private readonly uint[] _indices =
+        {
+            0,1,2,
+            2,3,0
+        };
+
+        private int _elementBufferObject;
+
+        private int _vertexBufferObject;
+
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+
+
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings) { }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            _shader.Use();
+
+            var vertexLocation = _shader.GetAttribLocation("vPosition");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, true, 3 * sizeof(float), 0);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.BindVertexArray(_vertexArrayObject);
+
+            _shader.Use();
+
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            var input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+    }
+}
